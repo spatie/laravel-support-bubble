ab1532dd3959f111b080344c4b64c126d126388c
freek <freek@spatie.be>
2021-08-18 00:33:01 +0200

    wip

--- resources/views/outer.blade.php
+++ resources/views/outer.blade.php
@@ -6,7 +6,7 @@
 <script>
     /**
      * Add JS to:
-     * - show inner form on submission
+     * - show inner form on click of button
      * - handle submission of inner form
      * - after submission, replace inner-form with response of HandleSupportFormSubmissionController
      */
