ae845fa95873f2b4e3b43947edcb87f0ba80d50a
Alex Vanderbist <alex.vanderbist@gmail.com>
2021-08-26 12:34:51 +0200

    wip

--- resources/views/includes/form.blade.php
+++ resources/views/includes/form.blade.php
@@ -1,12 +1,12 @@
 <div class="support-form-inner">
-    <p class="spatie-support-form__error pb-4 text-red-600 hidden"></p>
+    <p class="spatie-support-form__error pb-4 text-red-600" style="display: none"></p>
 
     <form method="post" action="{{ route(config('support-form.form_action_route')) }}" class="flex flex-col gap-2">
-        <label for="name">
+        <label for="name" class="{{ $name ? 'hidden' : '' }}">
             <input type="text" name="name" required placeholder="Your name" value="{{ $name }}" class="input">
         </label>
 
-        <label for="email">
+        <label for="email" class="{{ $email ? 'hidden' : '' }}">
             <input type="email" name="email" required placeholder="E-mail" value="{{ $email }}" class="input">
         </label>
 
