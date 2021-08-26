62c10e5561b6f6d8bc56be933737f2a0f99005e3
Alex Vanderbist <alex.vanderbist@gmail.com>
2021-08-26 14:52:26 +0200

    Get rid of configurable location

--- resources/views/components/support-bubble.blade.php
+++ resources/views/components/support-bubble.blade.php
@@ -1,9 +1,4 @@
-@php
-    $position = $positionY === 'bottom' ? 'bottom-0 ' : 'top-0 ';
-    $position .= $positionX === 'right' ? 'right-0 items-end ' : 'left-0';
-@endphp
-
-<div class="spatie-support-bubble | text-sm fixed {{$position}} z-10 flex-col m-4 gap-3" style="max-width: 340px; display: none;">
+<div class="spatie-support-bubble | text-sm fixed bottom-0 right-0 z-10 flex-col m-4 gap-3" style="max-width: 340px; display: none;">
     <div class="spatie-support-bubble__container | bg-white shadow-xl border border-gray-300 rounded p-6 transition transform translate-x-full opacity-0" style="display: none">
         <div class="spatie-support-bubble__form">
             @include('support-bubble::includes.form', compact('email'))
--- src/Components/SupportBubble.php
+++ src/Components/SupportBubble.php
@@ -8,10 +8,6 @@ use Str;
 
 class SupportBubble extends Component
 {
-    public string $positionY;
-
-    public string $positionX;
-
     public string $formAction;
 
     public string $email;
@@ -20,8 +16,6 @@ class SupportBubble extends Component
 
     public function __construct(string $position)
     {
-        $this->positionY = Str::contains($position, 'top') ? 'top' : 'bottom';
-        $this->positionX = Str::contains($position, 'left') ? 'left' : 'right';
         $this->formAction = route(config('support-bubble.form_action_route'));
         $this->email = config('support-bubble.prefill_logged_in_user') ? (optional(request()->user())->email ?? '') : '';
         $this->name = config('support-bubble.prefill_logged_in_user') ? (optional(request()->user())->name ?? '') : '';
