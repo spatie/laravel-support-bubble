e962ce228099e3dd389d21e0278dbb7487635656
Alex Vanderbist <alex.vanderbist@gmail.com>
2021-08-26 14:27:13 +0200

    wip

--- resources/views/includes/form.blade.php
+++ resources/views/includes/form.blade.php
@@ -3,7 +3,7 @@
     action="{{ route(config('support-bubble.form_action_route')) }}"
     class="flex flex-col gap-4"
 >
-    <p>
+    <p class="text-base">
         Contact Flare support for any questions, suggestions or bugs.<br/>We're available on all weekdays.
     </p>
 
@@ -11,23 +11,23 @@
 
     <label for="support-bubble-name" class="font-medium {{ $name ? 'hidden' : '' }}">
         Your name
-        <input type="text" name="name" id="support-bubble-name" required value="{{ $name }}" class="input">
+        <input type="text" name="name" id="support-bubble-name" required value="{{ $name }}" class="input text-base">
     </label>
 
     <label for="support-bubble-email" class="font-medium {{ $email ? 'hidden' : '' }}">
         E-mail address
         <input type="email" name="email" id="support-bubble-email" required value="{{ $email }}"
-               class="input">
+               class="input text-base">
     </label>
 
     <label for="support-bubble-subject" class="font-medium">
         Subject
-        <input type="text" name="subject" id="support-bubble-subject" required class="input">
+        <input type="text" name="subject" id="support-bubble-subject" required class="input text-base">
     </label>
 
     <label for="support-bubble-message" class="font-medium">
         How can we help?
-        <textarea name="message" id="support-bubble-message" rows="4" required class="input"></textarea>
+        <textarea name="message" id="support-bubble-message" rows="4" required class="input text-base"></textarea>
     </label>
 
     <button type="submit" class="button">Submit</button>
