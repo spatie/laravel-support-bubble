ea3b9e9bfbfe2983ba722b0d92a477bea65e2817
Alex Vanderbist <alex.vanderbist@gmail.com>
2021-09-06 13:33:48 +0200

    Fix pointer events on hidden support form

--- resources/views/components/support-bubble.blade.php
+++ resources/views/components/support-bubble.blade.php
@@ -1,6 +1,6 @@
-<div class="spatie-support-bubble | text-base fixed bottom-0 right-0 items-end z-10 flex-col m-4 gap-3"
+<div class="spatie-support-bubble | pointer-events-none text-base fixed bottom-0 right-0 items-end z-10 flex-col m-4 gap-3"
      style="max-width: 340px; display: none;">
-    <div class="spatie-support-bubble__container | bg-white shadow-xl border border-gray-300 rounded p-6 transition transform translate-x-full opacity-0">
+    <div class="spatie-support-bubble__container | pointer-events-auto bg-white shadow-xl border border-gray-300 rounded p-6 transition transform translate-x-full opacity-0">
         <div class="spatie-support-bubble__form">
             @include('support-bubble::includes.form')
         </div>
@@ -9,7 +9,7 @@
         </div>
     </div>
 
-    <div class="spatie-support-bubble__button">
+    <div class="spatie-support-bubble__button | pointer-events-auto">
         @include('support-bubble::includes.bubble')
     </div>
 </div>
