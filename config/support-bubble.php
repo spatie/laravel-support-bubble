36d9a79411915547f24d129bbaf36d13eb9dab2a
Alex Vanderbist <alex.vanderbist@gmail.com>
2021-09-07 10:35:12 +0200

    Add container classes to config

--- config/support-bubble.php
+++ config/support-bubble.php
@@ -12,7 +12,7 @@ return [
         'subject' => true,
         'message' => true,
     ],
-    
+
     /*
      * We'll send any chat bubble responses to this e-mail address.
      *
@@ -33,11 +33,12 @@ return [
      * the views of this package.
      */
     'classes' => [
+        'container' => 'text-base items-end z-30 flex-col m-4 gap-3',
         'bubble' => 'hidden sm:block | bg-purple-400 rounded-full shadow-lg w-14 h-14 text-white p-4',
         'input' => 'bg-gray-100 border border-gray-200 w-full max-w-full p-2 rounded-sm shadow-input text-gray-800 text-base',
         'button' => 'inline-flex place-center px-4 py-3 h-10 border-0 bg-purple-500 hover:bg-purple-600 active:bg-purple-600 overflow-hidden rounded-sm text-white leading-none no-underline',
     ],
-    
+
     /*
      * The default route and controller will be registered using this route name.
      * This is a good place to hook in your own route and controller if necessary.
@@ -46,7 +47,7 @@ return [
 
      /**
      * The positioning of the bubble and the form, change this between right-to-left and left-to-right.
-     * If you want to use RTL, you must have your layout set to RTL like this 
+     * If you want to use RTL, you must have your layout set to RTL like this
      * <html lang="ar-TN" dir="rtl">
      * By default, the value of this is left-to-right
      */
--- resources/views/components/support-bubble.blade.php
+++ resources/views/components/support-bubble.blade.php
@@ -1,4 +1,4 @@
-<div class="spatie-support-bubble | pointer-events-none text-base fixed bottom-0 {{ $direction === 'right-to-left' ? 'left-0' : 'right-0' }} items-end z-10 flex-col m-4 gap-3"
+<div class="spatie-support-bubble | pointer-events-none fixed bottom-0 {{ $direction === 'right-to-left' ? 'left-0' : 'right-0' }} {{ config('support-bubble.classes.container') }}"
      style="max-width: 340px; display: none;">
     <div class="spatie-support-bubble__container | pointer-events-auto bg-white shadow-xl border border-gray-300 rounded p-6 transition transform {{ $direction === 'right-to-left' ? '-translate-x-full' : 'translate-x-full' }} opacity-0">
         <div class="spatie-support-bubble__form">
