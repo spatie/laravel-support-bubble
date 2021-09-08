ad7c15bb70b4bdbb74454f36032ec030d96710aa
freek <freek@spatie.be>
2021-09-08 20:25:08 +0200

    wip

--- resources/views/mail/meta.blade.php
+++ resources/views/mail/meta.blade.php
@@ -1,26 +1,21 @@
 <h3>Meta</h3>
 <ul>
-    @if($name)
-        <li><strong>Name</strong>: {{ $name }}</li>
-    @endif
-
-    @if($email)
-        <li><strong>E-mail</strong>: {{ $email }}</li>
-    @endif
-
-    @if($subject)
-        <li><strong>Subject</strong>: {{ $subject }}</li>
-    @endif
-
-    @if($url)
-        <li><strong>URL</strong>: {{ $url }}</li>
-    @endif
-
-    @if($ip)
-        <li><strong>IP-address</strong>: {{ $ip }}</li>
-    @endif
-
-    @if($userAgent)
-        <li><strong>User-agent</strong>: {{ $userAgent }}</li>
-    @endif
+@if($name)
+<li><strong>Name</strong>: {{ $name }}</li>
+@endif
+@if($email)
+<li><strong>E-mail</strong>: {{ $email }}</li>
+@endif
+@if($subject)
+<li><strong>Subject</strong>: {{ $subject }}</li>
+@endif
+@if($url)
+<li><strong>URL</strong>: {{ $url }}</li>
+@endif
+@if($ip)
+<li><strong>IP-address</strong>: {{ $ip }}</li>
+@endif
+@if($userAgent)
+<li><strong>User-agent</strong>: {{ $userAgent }}</li>
+@endif
 </ul>
